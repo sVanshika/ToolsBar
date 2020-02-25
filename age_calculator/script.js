42c54ebc60917c200440372992e9e8f88e5324de
Vanshika Sharma <48361763+sVanshika@users.noreply.github.com>
2020-02-25 19:18:57 +0530

    Add files via upload

---
+++ age_calculator/script.js
@@ -0,0 +1,41 @@
+function dateFunction(){
+    var currentDate = new Date(),
+                    day = currentDate.getDate(),
+                    month = currentDate.getMonth() + 1,
+                    year = currentDate.getFullYear();
+    document.getElementById("outputDate").innerHTML = (day + "/" + month + "/" + year);
+}
+
+$(document).ready(function(){
+    $("#calculate").click(function(){
+        var mdate = $("#birth_date").val().toString();
+        var yearThen = parseInt(mdate.substring(0,4), 10);
+        var monthThen = parseInt(mdate.substring(5,7), 10);
+        var dayThen = parseInt(mdate.substring(8,10), 10);
+        
+        var today = new Date();
+        var birthday = new Date(yearThen, monthThen-1, dayThen);
+        
+        var differenceInMilisecond = today.valueOf() - birthday.valueOf();
+        
+        var year_age = Math.floor(differenceInMilisecond / 31536000000);
+        var day_age = Math.floor((differenceInMilisecond % 31536000000) / 86400000);
+        
+        
+        
+        var month_age = Math.floor(day_age/30);
+        
+        day_age = day_age % 30;
+        
+        if (isNaN(year_age) || isNaN(month_age) || isNaN(day_age)) {
+            $("#exact_age").text("Invalid birthday - Please try again!");
+        }
+        else {
+            $("#exact_age").html("You are <span id=\"age\">" + year_age + " years " + month_age + " months  </span> old");
+        }
+        if ((today.getMonth() == birthday.getMonth()) && (today.getDate() == birthday.getDate())) {
+            var person = prompt("Enter your name!");
+            alert("HAPPY BIRTHDAY "+person+" :)");
+        }
+    });
+});
